8a37ad66c031bb977799f951e75d4a2cee402fc3
Justin Lastra <lastra.justin@gmail.com>
2025-06-21 23:21:47 -0600

    finishing touches to modals

--- script/index.js
+++ script/index.js
@@ -1,12 +1,26 @@
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = document.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const profileNameElement = document.querySelector(".profile__name");
+const profileDescriptionElement = document.querySelector(
+  ".profile__description"
+);
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileNameElement.textContent;
+  editProfileDescriptionInput.value = profileDescriptionElement.textContent;
   editProfileModal.classList.add("modal_is-open");
 });
 
@@ -21,3 +35,16 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-open");
 });
+
+function handleEditProfileSubmit(event) {
+  event.preventDefault();
+  profileNameElement.textContent = editProfileNameInput.value;
+  console.log(editProfileNameInput.value);
+
+  profileDescriptionElement.textContent = editProfileDescriptionInput.value;
+  console.log(editProfileDescriptionInput.value);
+
+  editProfileModal.classList.remove("modal_is-open");
+}
+
+editProfileForm.addEventListener("submit", handleEditProfileSubmit);
